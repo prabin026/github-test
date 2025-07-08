fbd99e31e4fe3f5d17f5c76ffb13935a1bfffbea
prabin026 <dhakalprabin986@gmail.com>
2025-07-08 07:49:23 +0545

    third variable added for sub

--- sub.c
+++ sub.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 
-int subtract(int a, int b) {
-    return a - b;
+int subtract(int a, int b, int c) {
+    return a - b - c;
 }
 
 int main() {
-    int x, y;
-    printf("Enter two numbers: ");
-    scanf("%d %d", &x, &y);
+    int x, y, z;
+    printf("Enter three numbers: ");
+    scanf("%d %d %d", &x, &y, &z);
 
-    int result = subtract(x, y);
-    printf("After subtracting %d from %d, ", y, x);
+    int result = subtract(x, y, z);
+    printf("After subtracting %d and %d from %d, ", y, z, x);
     printf("Result: %d\n", result);
 
     return 0;
